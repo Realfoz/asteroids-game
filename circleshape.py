2f8a927652a5c25db5f3afba51f672f1442cfb5f
realfoz <scottpool@hotmail.com>
2025-03-16 18:49:16 +0000

    updated player, added asteroids and collision

--- circleshape.py
+++ circleshape.py
@@ -21,3 +21,12 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision(self, other):
+        distance = self.position.distance_to(other.position)
+        sum_radii = self.radius + other.radius
+        if distance <= sum_radii:
+            return True
+        else:
+            return False
+
--- main.py
+++ main.py
@@ -1,29 +1,48 @@
 import pygame
-from constants import *
+from constants import * # * imports everything, not best practice but its a small project
 from player import *
+from asteroid import *
+from asteroidfield import *
+from circleshape import *
 
+updatable = pygame.sprite.Group() # creates groups for things that need to update on screen each loop
+drawable = pygame.sprite.Group() # creates a group of things that need to be drawn on screen each loop
+asteroids = pygame.sprite.Group()
+Player.containers = (updatable, drawable,) # adds the player to relavent groups
+Asteroid.containers = (asteroids, updatable, drawable,) # adds asteroids to relavant groups
+AsteroidField.containers = (updatable,)
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    dt = 0
+    dt = 0 # delta time, keep the fps and clock speed tied together and is used in many movement calculations
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock() 
-    player_1 = player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    rocket = player_1.triangle()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # sets the parameters and creates the screen object
+    clock = pygame.time.Clock() #creates the clock object
+    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # creates the player object in the center of the screen
+    asteroidfield = AsteroidField() # creates teh asteroid field objects
 
     while True: # main game loop
         screen.fill((0,0,0)) # sets background to black
-        player_1.draw(screen)
-        player_1.update(dt)
+
+        updatable.update(dt) # upddates everything in the updatable group like players, asteroids, etc. each loop
+
+        for asteroid in asteroids:
+            if (player_1.collision(asteroid)) == True:
+                print ("Game Over!")
+                import sys
+                sys.exit()
+
+        for entity in drawable: # draws everything in the drawable group to screen each loop
+            entity.draw(screen)
+        
         pygame.display.flip() # updates screen
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # lets the program quit from the windows bar
                 return
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000 # updates dt based on 60 fps
     
 
 if __name__ == "__main__":
